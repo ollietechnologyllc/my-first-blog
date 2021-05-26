3186b993abb8165c50212922a4ad2553d663de8d
Morgan <habarakoo@gmail.com>
2021-05-25 20:28:12 -0600

    Added commenting platform

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
         path('drafts/', views.post_draft_list, name='post_draft_list'),
         path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
         path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+        path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+        path('post/<int:pk>/comment/approve/', views.comment_approve, name='comment_approve'),
+        path('post/<int:pk>/comment/remove/', views.comment_remove, name='comment_remove'),
         #path('post/<int:pk>/public/', views.make_public, name='make_public'),
 ]
 
